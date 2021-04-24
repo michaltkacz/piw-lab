6bf83b9c631c0121906b2a14e8f6d79e58a0e985
Michał Tkacz <58853722+michaltkacz@users.noreply.github.com>
2021-04-24 20:27:42 +0200

    migration to submodules

--- project-tinder/src/app/utils/stringToList.js
+++
@@ -1,9 +0,0 @@
-const stringToList = (str) => {
-  if (str === null || str === undefined) return '';
-
-  const clearedStr = str.toLowerCase().replace(/\s+/g, ' ').trim();
-  if (clearedStr === '') return [];
-  return clearedStr.split(' ');
-};
-
-export default stringToList;
